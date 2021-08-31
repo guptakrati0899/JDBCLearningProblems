fed92feb958a26124e68554990cc3369b4246fb8
KratiGupta <kratigupta8545@gmail.com>
2021-08-31 12:53:02 +0530

    Added Welcome Message Successfully

---
+++ main/java/com/jdbclearningproblems/JDBC.java
@@ -0,0 +1,7 @@
+package com.jdbclearningproblems;
+
+public class JDBC {
+    public static void main(String[] args) {
+        System.out.println("Welcome to master branch of JDBC Problems");
+    }
+}
