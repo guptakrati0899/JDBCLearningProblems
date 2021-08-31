28fbe027d2e0998d20fc57f47a0dba2508436eac
KratiGupta <kratigupta8545@gmail.com>
2021-08-31 13:11:17 +0530

    Added program to successfully connect java program to mysql server

--- main/java/com/jdbclearningproblems/JDBC.java
+++ main/java/com/jdbclearningproblems/JDBC.java
@@ -1,7 +1,40 @@
 package com.jdbclearningproblems;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+
 public class JDBC {
     public static void main(String[] args) {
-        System.out.println("Welcome to master branch of JDBC Problems");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "rustygupta";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot fine the driver in the Classpath!", e);
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to the Database: "+jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is Successfully Established! "+connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println("  "+driverClass.getClass().getName());
+        }
     }
 }
+
+
